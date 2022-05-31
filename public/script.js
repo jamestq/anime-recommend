39e4ea73447ca182538bd20275b4f42871ca89dc
UTQNG <57053746+UTQNG@users.noreply.github.com>
2022-05-31 12:03:23 +1000

    add barebone dropdown list and functions to retrieve genres at page load.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
-alert('hello world');
+import {populateGenresDropDown} from './helper.js';
+
+const jikanBaseUrl = "https://api.jikan.moe/v4";
+
+async function getGenres(){
+    let genreRequestEndPoint = "/genres/anime";
+    let urlToFetch = jikanBaseUrl + genreRequestEndPoint;
+    try{
+        let response = await fetch(urlToFetch);
+        if(response.ok){
+            let jsonResponse = await response.json();
+            let genres = jsonResponse.data;  
+            return genres;
+        }
+    }catch(error){
+        console.log(error);
+    }
+}
+
+getGenres().then(populateGenresDropDown);
